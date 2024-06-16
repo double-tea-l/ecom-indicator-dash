f4467a662a64d0c86d37b5224469fd6aaa5e6f6b
Double Tea <106457559+double-tea-l@users.noreply.github.com>
2024-06-16 01:37:50 -0700

    set up structure

--- data/ecom_indicators_data.py
+++ data/ecom_indicators_data.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import json
 import prettytable
+import utils
 
 class get_major_indicators():
     
@@ -16,32 +17,49 @@ class get_major_indicators():
         self.real_gdp = self.get_real_gdp()
         self.gdp_pct_change = self.get_gdp_pct_change()
         
-        def fetch_data(self):
-            
-            series_id = self.series_id
-            
-            """Fetch observations for a FRED series and save to a DataFrame."""
-            url = f"https://api.stlouisfed.org/fred/series/observations"
-            
-            api_key = "9648695895d3facac4fba9c7cb834427"
-            
-            params = {
-                "series_id": series_id,
-                "api_key": api_key,
-                "file_type": "json"
-            }
-            response = requests.get(url, params=params)
-            
+    def get_gdp(self):
 
-            data = response.json()
-            observations = data.get('observations', [])
-            # Create DataFrame
-            df = pd.DataFrame(observations)
-            # Display the DataFrame
-            
-            return df
+        # Units:  Billions of Dollars, Seasonally Adjusted Annual Rate
+        # Frequency:  Quarterly
+        # BEA Account Code: A191RC   
+        series_id = "GDP"
 
+        us_gdp = utils.fetch_data(series_id)
+        us_gdp = us_gdp[['date','value']]
+        us_gdp.rename(columns = {'value': 'GDP'}, inplace= True)
         
-
-            
+        # GDP breakdown: https://fred.stlouisfed.org/release/tables?rid=53&eid=13026&od=#
+        return us_gdp
     
+    def get_real_gdp(self):
+
+        # Real Gross Domestic Product (Inflation Adjusted)
+        # Units:  Billions of Chained 2017 Dollars, Seasonally Adjusted Annual Rate
+        # Frequency:  Quarterly
+        # BEA Account Code: A191RX
+
+        series_id = "GDPC1"
+
+        us_real_gdp = utils.fetch_data(series_id)
+        us_real_gdp = us_real_gdp[['date','value']]
+
+        us_real_gdp.rename(columns = {'value': 'Real_GDP'}, inplace= True)
+        
+        # Real gross domestic product is the inflation adjusted value of the goods and services produced by labor and property located in the United States.
+        # For more information see the Guide to the National Income and Product Accounts of the United States (NIPA). For more information, please visit the Bureau of Economic Analysis.
+        
+        return us_real_gdp
+    
+    def get_gdp_pct_change(self):
+
+        #  Gross Domestic Product: Implicit Price Deflator
+        # Units:  Percent Change from Preceding Period, Seasonally Adjusted Annual Rate
+        # Frequency:  Quarterly
+        # BEA Account Code: A191RI
+        series_id = "A191RI1Q225SBEA"
+
+        us_gdp_pct_change = utils.fetch_data(series_id)
+        us_gdp_pct_change = us_gdp_pct_change[['date','value']]
+        us_gdp_pct_change.rename(columns = {'value': 'GDP_pct_change'}, inplace= True)
+        
+        return us_gdp_pct_change
---
+++ data/indicators.py
@@ -0,0 +1,36 @@
+
+import pandas as pd
+import ecom_indicators_data as ind_data
+import math
+
+
+def df_gdp():
+    
+    #inputs
+    us_gdp_pct_change = ind_data.us_gdp_pct_change
+    us_gdp = ind_data.us_gdp
+    us_real_gdp =  ind_data.us_real_gdp
+    
+    # format
+    df_gdp = pd.merge(us_gdp_pct_change, us_gdp, on = 'date', how = 'outer')
+    df_gdp = pd.merge(df_gdp, us_real_gdp, on = 'date', how = 'outer')
+    df_gdp = df_gdp.sort_values(by='date', ascending= True).reset_index(drop = True)
+    df_gdp['date'] = pd.to_datetime(df_gdp['date'])
+    # Function to convert date to quarter
+    def date_to_quarter(date):
+        return f"{date.year} Q{((date.month - 1) // 3) + 1}"
+    # Apply the function to the dataframe
+    df_gdp['Quarter'] = df_gdp['date'].apply(date_to_quarter)
+    df_gdp = df_gdp[df_gdp['date']>='1948-01-01']
+    # Convert 'GDP' and 'Real_GDP' columns to numeric, coercing errors
+    df_gdp['GDP'] = pd.to_numeric(df_gdp['GDP'], errors='coerce')
+    df_gdp['Real_GDP'] = pd.to_numeric(df_gdp['Real_GDP'], errors='coerce')
+
+    # Round up GDP values to integers
+    df_gdp['GDP'] = df_gdp['GDP'].apply(lambda x: math.ceil(x) if pd.notnull(x) else x)
+    df_gdp['Real_GDP'] = df_gdp['Real_GDP'].apply(lambda x: math.ceil(x) if pd.notnull(x) else x)
+    
+    return df_gdp
+
+
+
