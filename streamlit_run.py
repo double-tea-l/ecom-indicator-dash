3ea40e2521b1ea552f538bd184b1c0ba6ee0c988
Double Tea <106457559+double-tea-l@users.noreply.github.com>
2024-06-17 01:45:16 -0700

    q

--- streamlit_run.py
+++ streamlit_run.py
@@ -1,14 +1,11 @@
 
 import streamlit as st
-
-
-st.set_page_config(initial_sidebar_state="collapsed")
-
-
 from streamlit_navigation_bar import st_navbar
 import pages as pg
 import os
 
+
+st.set_page_config(initial_sidebar_state="collapsed")
 # Set the page config at the start of the main script
 # st.set_page_config(
 #     page_title="US Population Dashboard",
